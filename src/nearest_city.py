b24531437c1e60eb7b9cde07fb14e4fbf00d1e86
Nootwin <nolanmurray06@gmail.com>
2025-11-09 16:03:28 -0500

    nej

--- src/nearest_city.py
+++ src/nearest_city.py
@@ -13,8 +13,8 @@ def list_of_nearby_areas(lat, longi, list):
             if not(location in list):
                 list.append(location)
 
-def list_of_nearby_areas_with_api(lat, longi, list):
-    api = WeatherBit_Caller
+def list_of_nearby_areas_with_api(lat, longi, list, ttk, tab):
+    api = WeatherBit_Caller()
 
 
     loc = Nominatim(user_agent="sunnyside_app")
@@ -22,36 +22,33 @@ def list_of_nearby_areas_with_api(lat, longi, list):
         for j in range(-1, 2, 1):
             print(i, j)
             location = loc.reverse(str(i/2+lat) + ", " + str(j/2+longi))
-            data = api.request(api, i/2+lat, j/2+longi)
+            data = api.request(i/2+lat, j/2+longi)
             print(location)
             print(data)
             if not(location in list) and data["alerts"] != []:
                 list.append([location, data["alerts"][0]["severity"]])
+                m = ttk.Label(tab, text = location + " " + data["alerts"][0]["severity"], font=("Browallia new", 40))
+                m.pack(padx=100, pady=10)
+                
 
-
-if  __name__ == "__main__":
-    l = []
-    loc = Nominatim(user_agent="sunnyside_app")
-    getLoc = loc.geocode("Toronto, Canada")
-    list_of_nearby_areas_with_api(getLoc.latitude, getLoc.longitude, l)
-    print(l)
 
 def nearest_tab(ttk, tab):
     ttk.Label(tab, 
             text = "Nearby Areas", font=("Browallia new", 24)).pack(padx=100, pady=(0,100))
-    ttk.Button(tab, text="Generate", command=lambda : nearest_city_create).pack(padx = "100", pady = 10)
+    ttk.Button(tab, text="Generate", command=lambda : nearest_city_create(ttk, tab)).pack(padx = "100", pady = 10)
     
 
-    l = []
-    list_of_nearby_areas_with_api(lat, longi, l)
+    
 
-    for i in l:
-        ttk.Label(tab, 
-            text = i[0] + " " + i[1], font=("Browallia new", 40)).pack(padx=100, pady=(0,100))
+    
 
-def nearest_city_create(ttk):
+def nearest_city_create(ttk, tab):
+    
     file = open("sunny_data\location.txt", "r")
     latlongtext = file.readline();
     latlongarr = latlongtext.split(";")
     lat = float(latlongarr[0])
     longi = float(latlongarr[1])
+    list_of_nearby_areas_with_api(lat, longi, [], ttk, tab)
+
+        
--- src/welcome_tab.py
+++ src/welcome_tab.py
@@ -43,7 +43,7 @@ def set_to_current(locText):
     if (g.latlng == None):
         locText.config(text = "Couldnt't get address")
         return
-    file = open("../sunny_data/location.txt", "w")
+    file = open("sunny_data/location.txt", "w")
     file.write(str(latlng[0]))
     file.write(";")
     file.write(str(latlng[1]))
@@ -58,7 +58,7 @@ def set_to_custom(custom, locText):
     if (getLoc == None):
         locText.config(text = "Couldn't set location with input: " + custom.get())
         return
-    file = open("../sunny_data/location.txt", "w")
+    file = open("sunny_data/location.txt", "w")
     file.write(str(getLoc.latitude))
     file.write(";")
     file.write(str(getLoc.longitude))
