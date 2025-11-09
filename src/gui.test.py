2cc14624ca427f1a3722a67f9631a013ccdfee7a
Nootwin <nolanmurray06@gmail.com>
2025-11-09 10:41:08 -0500

    basic gui example

---
+++ src/gui.test.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+class MyTkinterApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Simple Tkinter App")
+
+        self.label_text = tk.StringVar()
+        self.label_text.set("Hello, Tkinter!")
+
+        self.label = tk.Label(master, textvariable=self.label_text, font=("Arial", 16))
+        self.label.pack(pady=20)
+
+        self.button = tk.Button(master, text="Click Me", command=self.on_button_click)
+        self.button.pack(pady=10)
+
+    def on_button_click(self):
+        self.label_text.set("Button clicked!")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MyTkinterApp(root)
+    root.mainloop()
